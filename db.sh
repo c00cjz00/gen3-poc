8ec0af7a831a1ef505780c45466372ebcdd6ef4e
c00cjz00 <summerhill001@gmail.com>
2022-04-25 17:10:23 +0800

    Update db.sh

--- db.sh
+++ db.sh
@@ -1,4 +1,3 @@
-
 rm -rf db
 mkdir -p db/mysql
 mkdir -p db/psql
@@ -8,7 +7,7 @@ docker exec -it  postgres su postgres bash -c 'pg_dump --format p --inserts inde
 docker exec -it  postgres su postgres bash -c 'pg_dump --format p --inserts metadata > /tmp/metadata.sql'
 docker exec -it  postgres su postgres bash -c 'pg_dump --format p --inserts metadata_db > /tmp/metadata_db.sql'
 
-docker cp postgres:/tmp/arborist_db.sql db/psql/arborist_db.sql
+docker cp postgres:/tmp/arborist_db.sql db/psql/arborist_db.sql 
 docker cp postgres:/tmp/fence_db.sql db/psql/fence_db.sql
 docker cp postgres:/tmp/indexd_db.sql db/psql/indexd_db.sql
 docker cp postgres:/tmp/metadata.sql db/psql/metadata.sql
@@ -27,8 +26,47 @@ php pg2mysql_cli.php db/psql/metadata.sql db/mysql/metadata.sql InnoDB
 php pg2mysql_cli.php db/psql/metadata_db.sql db/mysql/metadata_db.sql InnoDB
 
 
+sed -i 's/public./gen3_arborist_db./g' db/mysql/arborist_db.sql
+sed -i 's/public./gen3_fence_db./g' db/mysql/fence_db.sql
+sed -i 's/public./gen3_indexd_db./g' db/mysql/indexd_db.sql
+sed -i 's/public./gen3_metadata./g' db/mysql/metadata.sql
+sed -i 's/public./gen3_metadata_db./g' db/mysql/metadata_db.sql
 
+sed -i 's/ TYPE=InnoDB//g' db/mysql/arborist_db.sql
+sed -i 's/ TYPE=InnoDB//g' db/mysql/fence_db.sql
+sed -i 's/ TYPE=InnoDB//g' db/mysql/indexd_db.sql
+sed -i 's/ TYPE=InnoDB//g' db/mysql/metadata.sql
+sed -i 's/ TYPE=InnoDB//g' db/mysql/metadata_db.sql
 
+sed -i "s/jsonb DEFAULT '{}'/text/g" db/mysql/arborist_db.sql
+sed -i "s/jsonb DEFAULT '{}'/text/g" db/mysql/fence_db.sql
+sed -i "s/jsonb DEFAULT '{}'/text/g" db/mysql/indexd_db.sql
+sed -i "s/jsonb DEFAULT '{}'/text/g" db/mysql/metadata.sql
+sed -i "s/jsonb DEFAULT '{}'/text/g" db/mysql/metadata_db.sql
 
+sed -i 's/jsonb/text/g' db/mysql/arborist_db.sql
+sed -i 's/jsonb/text/g' db/mysql/fence_db.sql
+sed -i 's/jsonb/text/g' db/mysql/indexd_db.sql
+sed -i 's/jsonb/text/g' db/mysql/metadata.sql
+sed -i 's/jsonb/text/g' db/mysql/metadata_db.sql
 
+sed -i 's/text\[\]/text/g' db/mysql/arborist_db.sql
+sed -i 's/text\[\]/text/g' db/mysql/fence_db.sql
+sed -i 's/text\[\]/text/g' db/mysql/indexd_db.sql
+sed -i 's/text\[\]/text/g' db/mysql/metadata.sql
+sed -i 's/text\[\]/text/g' db/mysql/metadata_db.sql
+
+docker cp db.sql mysql:/tmp/db.sql
+docker cp db/mysql/arborist_db.sql mysql:/tmp/arborist_db.sql 
+docker cp db/mysql/fence_db.sql mysql:/tmp/fence_db.sql
+docker cp db/mysql/indexd_db.sql mysql:/tmp/indexd_db.sql
+docker cp db/mysql/metadata.sql mysql:/tmp/metadata.sql 
+docker cp db/mysql/metadata_db.sql mysql:/tmp/metadata_db.sql
+
+docker exec -it  mysql bash -c 'mysql -u root -padmin -f < /tmp/db.sql'
+docker exec -it  mysql bash -c 'mysql -u root -padmin -f gen3_arborist_db < /tmp/arborist_db.sql'
+docker exec -it  mysql bash -c 'mysql -u root -padmin -f gen3_fence_db < /tmp/fence_db.sql'
+docker exec -it  mysql bash -c 'mysql -u root -padmin -f gen3_indexd_db < /tmp/indexd_db.sql'
+docker exec -it  mysql bash -c 'mysql -u root -padmin -f gen3_metadata < /tmp/metadata.sql'
+docker exec -it  mysql bash -c 'mysql -u root -padmin -f gen3_metadata_db < /tmp/metadata_db.sql'
 
