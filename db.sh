5f2d8956355dd3950b17e94b7c2e2fb55b2fb24f
c00cjz00 <summerhill001@gmail.com>
2022-04-25 15:51:32 +0800

    Update db.sh

--- db.sh
+++ db.sh
@@ -1,3 +1,5 @@
+
+rm -rf db
 mkdir -p db/mysql
 mkdir -p db/psql
 docker exec -it  postgres su postgres bash -c 'pg_dump --format p --inserts arborist_db > /tmp/arborist_db.sql'
@@ -14,14 +16,17 @@ docker cp postgres:/tmp/metadata_db.sql db/psql/metadata_db.sql
 
 
 sudo apt install php7.4-cli -y
-cd db
-wget http://www.lightbox.ca/pg2mysql/pg2mysql-1.9.tar.bz2
-tar -xjvf pg2mysql-1.9.tar.bz2
-php pg2mysql-1.9/pg2mysql_cli.php psql/arborist_db.sql mysql/arborist_db.sql InnoDB
-php pg2mysql-1.9/pg2mysql_cli.php psql/fence_db.sql mysql/fence_db.sql InnoDB
-php pg2mysql-1.9/pg2mysql_cli.php psql/indexd_db.sql mysql/indexd_db.sql InnoDB
-php pg2mysql-1.9/pg2mysql_cli.php psql/metadata.sql mysql/metadata.sql InnoDB
-php pg2mysql-1.9/pg2mysql_cli.php psql/metadata_db.sql mysql/metadata_db.sql InnoDB
+#cd db
+#wget http://www.lightbox.ca/pg2mysql/pg2mysql-1.9.tar.bz2
+#tar -xjvf pg2mysql-1.9.tar.bz2
+#php pg2mysql_cli.php db/psql/fence_db.sql db/mysql/fence_db.sql InnoDB
+php pg2mysql_cli.php db/psql/arborist_db.sql db/mysql/arborist_db.sql InnoDB
+php pg2mysql_cli.php db/psql/fence_db.sql db/mysql/fence_db.sql InnoDB
+php pg2mysql_cli.php db/psql/indexd_db.sql db/mysql/indexd_db.sql InnoDB
+php pg2mysql_cli.php db/psql/metadata.sql db/mysql/metadata.sql InnoDB
+php pg2mysql_cli.php db/psql/metadata_db.sql db/mysql/metadata_db.sql InnoDB
+
+
 
 
 
